8f698adc4ea91ae9af3fefb8b4e0754fcc52ce9c
Lucas Figueiredo <olucasfg@outlook.com>
2025-01-16 15:25:55 -0300

    validando o input e obtendo a moeda selecionada

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// obtenos os elementos do formulário
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+
+// manipulando o input amount para receber somente números
+amount.addEventListener("input", ()=>{
+  const hasCharactersRegex = /\D+/g
+
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+// capturando o evento de submit (enviar) do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  console.log(currency.value)
+}
+
